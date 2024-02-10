c99f2f273d29a86aa183bc2e12c0b9903ca20a7e
Max Emrich <maxfisherbear@gmail.com>
2024-02-09 21:14:57 -0800

    fixed stories page

--- routes/stories-routes.js
+++ routes/stories-routes.js
@@ -6,21 +6,27 @@ const path = require("path");
 const { file } = require("googleapis/build/src/apis/file");
 
 router.get("/", (req, res) => {
-  res.render("stories");
-});
-
-router.get("/select-story", (req, res) => {});
-
-router.get("/display-stories", (req, res) => {
   db.query("SELECT story_name FROM stories", (err, result) => {
     if (err) {
       console.log(err);
     }
-    console.log(result);
-    const name = result[0].story_name;
-    console.log(name);
-    res.render("article-template", { name: name });
+    console.log(result[0].story_name);
+    res.render("stories", { result: result });
   });
 });
+
+router.get("/select-story", (req, res) => {});
+
+// router.get("/display-stories", (req, res) => {
+//   db.query("SELECT story_name FROM stories", (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//     const name = result[0].story_name;
+//     console.log(name);
+//     res.render("article-template", { name: name });
+//   });
+// });
 
 module.exports = router;
