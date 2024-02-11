8f0ee270aae4a6d0b3d6a44f04525cab6917439e
Max Emrich <maxfisherbear@gmail.com>
2024-02-11 11:10:09 -0800

    added html files back to views

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.set("views", __dirname);
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("index");
