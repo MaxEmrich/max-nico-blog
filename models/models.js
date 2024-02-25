9519b53133d5f42abe638a5730693116fb587754
Max Emrich <maxfisherbear@gmail.com>
2024-02-25 11:50:26 -0800

    oh my lord

--- models/models.js
+++ models/models.js
@@ -1,12 +1,23 @@
 const mysql = require("mysql2");
 const config = require("../config/env_config");
 
+// (async () => {
+//   try {
+//     const [results, metadata] = await sequelize.query("SELECT NOW()");
+//     console.log(results);
+//   } catch (err) {
+//     console.error("error executing query:", err);
+//   } finally {
+//     await sequelize.close();
+//   }
+// })();
+
 // Create a connection pool
 const db = mysql.createConnection({
   host: `${config.SQL_SERVER_IP}`,
   user: `${config.SQL_USERNAME}`,
   password: `${config.SQL_PASSWORD}`,
-  database: "node_app_database",
+  database: "sql3682952",
 });
 
 // Connect to the database
