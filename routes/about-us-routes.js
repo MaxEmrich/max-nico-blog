64c959e26e23c29c9de3be5c5915ae51691083df
Max Emrich <maxfisherbear@gmail.com>
2024-03-27 15:05:25 -0700

    1

--- routes/about-us-routes.js
+++ routes/about-us-routes.js
@@ -1,9 +1,9 @@
-const { render } = require("ejs");
 const express = require("express");
 const router = express.Router();
+const { render } = require("ejs");
 
 router.get("/", (req, res) => {
-  res.render("about-us.ejs");
+  res.render("about-us");
 });
 
 module.exports = router;
